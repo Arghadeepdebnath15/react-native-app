f93c7c2df1d676cf01bce9a1f038bf9daae76fec
Arghadeepdebnath15 <arghadeepdebnath15@gmail.com>
2025-04-10 07:39:36 +0530

    wsw

--- product-review-app/src/components/SuccessPopup.js
+++ product-review-app/src/components/SuccessPopup.js
@@ -5,7 +5,7 @@ const SuccessPopup = ({ message, onClose }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       onClose();
-    }, 3000); // Auto close after 3 seconds
+    }, 2900); // Auto close after 3 seconds
 
     return () => clearTimeout(timer);
   }, [onClose]);
