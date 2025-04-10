7dda877e8ca8c9ca50716e4cb8bb5177308ddf2e
Arghadeepdebnath15 <arghadeepdebnath15@gmail.com>
2025-04-10 20:03:13 +0530

    uguxvdv

--- product-review-app/src/components/ChatList.js
+++ product-review-app/src/components/ChatList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { collection, query, getDocs, orderBy, where, onSnapshot, or, updateDoc } from 'firebase/firestore';
 import { db } from '../firebase/config';
